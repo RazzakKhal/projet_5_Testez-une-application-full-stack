b380060ab6f01596d32eba3c582dfca38b801e52
KHALFALLAH Razzak (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDA TP DOI) <9506532m@commun.ad.sncf.fr>
2024-08-01 02:39:05 +0200

    feat(tu2): add of tu

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,47 +1,49 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatInputHarness } from '@angular/material/input/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
-
+import {HarnessLoader} from '@angular/cdk/testing';
 import { RegisterComponent } from './register.component';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { SessionService } from 'src/app/services/session.service';
 import { of, throwError } from 'rxjs';
-
+import {TestbedHarnessEnvironment} from '@angular/cdk/testing/testbed';
+import { By } from '@angular/platform-browser';
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
   let router: Router;
   let authService: AuthService;
-
+  let loader: HarnessLoader;
   /**
    * Mock des Injections de dépendances
    */
 
   // Mock du Router
 
-  const routerMock ={
-      navigate : jest.fn()
-    }
+  const routerMock = {
+    navigate: jest.fn()
+  }
 
   // Mock du AuthService
 
   const authServiceMock = {
-      register : jest.fn()
-    }
+    register: jest.fn()
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
-      providers : [
-        {provide : Router, useValue : routerMock},
-        {provide : AuthService, useValue : authServiceMock}
+      providers: [
+        { provide: Router, useValue: routerMock },
+        { provide: AuthService, useValue: authServiceMock }
       ],
       imports: [
         BrowserAnimationsModule,
@@ -57,13 +59,14 @@ describe('RegisterComponent', () => {
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+    loader = TestbedHarnessEnvironment.loader(fixture);
     fixture.detectChanges();
     router = TestBed.inject(Router)
     authService = TestBed.inject(AuthService);
     (authService.register as jest.Mock).mockReturnValue(of('test'))
   });
 
-  afterEach(()=> {
+  afterEach(() => {
     jest.resetAllMocks();
   })
 
@@ -72,11 +75,11 @@ describe('RegisterComponent', () => {
     expect(component).toBeTruthy();
   });
 
-   /**
-   * tests de la méthode submit
-   */
+  /**
+  * tests de la méthode submit
+  */
 
-   it('should submit with success', () =>{
+  it('should submit with success', () => {
     const navigateSpy = jest.spyOn(router, 'navigate')
     const loginSpy = jest.spyOn(authService, 'register');
     component.submit();
@@ -99,36 +102,52 @@ describe('RegisterComponent', () => {
 
 
 
-  describe('tests integrations', () => {
+
   /**
-   * test validators form
+   * test d'integrations
+   *
+   * https://angularexperts.io/blog/material-component-testing  pour tester les composants angular material car ils ne se chargent pas de suite
+   * exemple ici: https://github.com/angular/components/blob/main/src/components-examples/material/card/card-harness/card-harness-example.spec.ts
+   *
+   *  (await loader.getAllHarnesses(MatInputHarness)).forEach((item : any) => console.log(item))    FONCTIONNE
+   *  fixture.debugElement.query(By.css('input')).nativeElement.forEach((item : any) => {console.log(item)})    NE FONCTIONNE PAS
    */
-  const dom = fixture.nativeElement;
-  it('should invalidate form', () => {
+  it('should invalidate form',async () => {
+    const firstNameInput = await loader.getHarness(MatInputHarness.with({ placeholder: 'First name' }));
+    const lastNameInput = await loader.getHarness(MatInputHarness.with({ placeholder: 'Last name' }));
+    const emailInput = await loader.getHarness(MatInputHarness.with({ placeholder: 'Email' }));
+    const passwordInput = await loader.getHarness(MatInputHarness.with({ placeholder: 'Password' }));
 
+    await firstNameInput.setValue('');
+    await lastNameInput.setValue('');
+    await emailInput.setValue('');
+    await passwordInput.setValue('');
 
-
-    dom.querySelector('input[placeholder="email"]').value = ""
-    dom.querySelector('input[placeholder="firstName"]').value = ""
-    dom.querySelector('input[placeholder="lastName"]').value = ""
-    dom.querySelector('input[placeholder="password"]').value = ""
     fixture.detectChanges();
 
     expect(component.form.invalid).toBeTruthy();
 
+
   });
 
-  fit('should validate form', () => {
-    dom.querySelector('input[placeholder="email"]').value = "khalfallah.razzak@gmail.com"
-    dom.querySelector('input[placeholder="firstName"]').value = "razzak"
-    dom.querySelector('input[placeholder="lastName"]').value = "khalfallah"
-    dom.querySelector('input[placeholder="password"]').value = "abdel2020"
+  it('should validate form',async () => {
+
+    const firstNameInput = await loader.getHarness(MatInputHarness.with({ placeholder: 'First name' }));
+    const lastNameInput = await loader.getHarness(MatInputHarness.with({ placeholder: 'Last name' }));
+    const emailInput = await loader.getHarness(MatInputHarness.with({ placeholder: 'Email' }));
+    const passwordInput = await loader.getHarness(MatInputHarness.with({ placeholder: 'Password' }));
+
+    await firstNameInput.setValue('razzak');
+    await lastNameInput.setValue('khalfallah');
+    await emailInput.setValue('khalfallah.razzak@gmail.com');
+    await passwordInput.setValue('abdel2020');
+
     fixture.detectChanges();
 
     expect(component.form.valid).toBeTruthy();
 
   });
-  })
+
 
 
 
