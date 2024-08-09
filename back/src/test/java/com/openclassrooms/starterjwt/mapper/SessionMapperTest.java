47669e6d73370ef47e83fd0d2b249531e33a4f9f
KHALFALLAH Razzak (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDA TP DOI) <9506532m@commun.ad.sncf.fr>
2024-08-09 02:44:44 +0200

    feat(testback): repositoryTests done

---
+++ back/src/test/java/com/openclassrooms/starterjwt/mapper/SessionMapperTest.java
@@ -0,0 +1,7 @@
+package com.openclassrooms.starterjwt.mapper;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionMapperTest {
+
+}
---
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -0,0 +1,7 @@
+package com.openclassrooms.starterjwt.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SessionServiceTest {
+
+}
