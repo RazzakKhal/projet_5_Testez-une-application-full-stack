368063704c9df81ecafbee53be4d8958aac2378f
KHALFALLAH Razzak (SNCF / DGA NUMERIQUE / e.SNCF Sol.D2D DDA TP DOI) <9506532m@commun.ad.sncf.fr>
2024-08-12 18:44:43 +0200

    feat(tuback): services tests Done

--- back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -1,7 +1,122 @@
 package com.openclassrooms.starterjwt.services;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.openclassrooms.starterjwt.models.Session;
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.SessionRepository;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class SessionServiceTest {
 
+    @Autowired
+    SessionService sessionService;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @MockBean
+    SessionRepository sessionRepository;
+    @MockBean
+    UserRepository userRepository;
+
+    User user;
+    @BeforeEach
+    void setUp(){
+
+        user = new User("razzaktest@example.com", "razz", "khal", passwordEncoder.encode("password123"), false);
+    }
+
+    @Test
+    void testCreate_Success(){
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        Session session = new Session();
+        session.setId(1L);
+        session.setName("Test Session");
+        session.setDescription("description test");
+        session.setUsers(users);
+
+        when(sessionRepository.save(any(Session.class))).thenReturn(session);
+        Session createdSession = sessionService.create(session);
+
+        assertEquals(session.getName(), createdSession.getName());
+        verify(sessionRepository,  times(1)).save(session);
+
+    }
+    @Test
+    void testDelete_Success(){
+        doNothing().when(sessionRepository).deleteById(anyLong());
+        sessionService.delete(1L);
+        verify(sessionRepository, times(1)).deleteById(1L);
+
+    }
+    @Test
+    void testFindAll_Success(){
+        List<Session> sessions = new ArrayList<>();
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        Session session = new Session();
+        session.setId(1L);
+        session.setName("Test Session");
+        session.setDescription("description test");
+        session.setUsers(users);
+
+        sessions.add(session);
+
+        when(sessionRepository.findAll()).thenReturn(sessions);
+        List<Session> resultat= sessionService.findAll();
+
+
+        verify(sessionRepository, times(1)).findAll();
+        assertEquals(sessions.size(), resultat.size());
+    }
+    @Test
+    void testGetById_Success(){
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        Session session = new Session();
+        session.setId(1L);
+        session.setName("Test Session");
+        session.setDescription("description test");
+        session.setUsers(users);
+
+        when(sessionRepository.findById(1L)).thenReturn(Optional.of(session));
+
+        Session foundSession = sessionService.getById(1L);
+
+        assertEquals("Test Session", foundSession.getName());
+        verify(sessionRepository, times(1)).findById(1L);
+    }
+    @Test
+    void testUpdate_Success(){
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        Session session = new Session();
+        session.setId(1L);
+        session.setName("Test Session");
+        session.setDescription("description test");
+        session.setUsers(users);
+
+        when(sessionRepository.save(any(Session.class))).thenReturn(session);
+
+        Session updatedSession = sessionService.update(1L, session);
+
+        assertEquals("Test Session", updatedSession.getName());
+        assertEquals(1L, updatedSession.getId());
+        verify(sessionRepository, times(1)).save(session);
+    }
 }
--- back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/UserServiceTest.java
@@ -1,7 +1,60 @@
 package com.openclassrooms.starterjwt.services;
 
+import com.openclassrooms.starterjwt.models.User;
+import com.openclassrooms.starterjwt.repository.UserRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
-
+@SpringBootTest
 class UserServiceTest {
+    @Autowired
+    UserRepository userRepository;
 
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @BeforeEach
+    void setUp(){
+        // création d'un utiliseur en bdd
+        User user = new User("razzaktest@example.com", "razz", "khal", passwordEncoder.encode("password123"), false);
+        userRepository.saveAndFlush(user);
+
+    }
+    @AfterEach
+    void setDown(){
+        Optional<User> user = userRepository.findByEmail("razzaktest@example.com");
+        if(user.isPresent()){
+            userRepository.deleteById(user.get().getId());
+        }
+    }
+    @Test
+    void testDeleteById_Success(){
+        Optional<User> user = userRepository.findByEmail("razzaktest@example.com");
+        if(user.isPresent()){
+            assertTrue(userRepository.findById(user.get().getId()).isPresent());
+            userService.delete(user.get().getId());
+            assertFalse(userRepository.findById(user.get().getId()).isPresent());
+        }
+
+    }
+    @Test
+    void testFindById(){
+        Optional<User> user = userRepository.findByEmail("razzaktest@example.com");
+        if(user.isPresent()){
+            Optional<User> user2 = userRepository.findById(user.get().getId());
+            assertTrue(user2.isPresent());
+            assertEquals(user2.get().getEmail(), user.get().getEmail());
+        }
+
+    }
 }
